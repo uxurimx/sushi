fd845278ff721dfe86309d86d875dd09b1b9554f
Jesus torres <sh0ke87@gmail.com>
2025-11-15 19:36:50 -0600

    animations

--- app.js
+++ app.js
@@ -520,15 +520,15 @@ document.addEventListener('DOMContentLoaded', () => {
                         
                     </div>
                     <div class="flex items-center space-x-3">
-                        <button class="cart-quantity-btn" data-id="${item.id}" data-action="decrease">
+                        <button class="cart-quantity-btn tap-press" data-id="${item.id}" data-action="decrease">
                             <svg class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4"></path></svg>
                         </button>
                         <span class="font-bold w-5 text-center dark:text-white">${item.quantity}</span>
-                        <button class="cart-quantity-btn" data-id="${item.id}" data-action="increase">
+                        <button class="cart-quantity-btn tap-press" data-id="${item.id}" data-action="increase">
                             <svg class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
                         </button>
                     </div>
-                    <button class="cart-remove-btn p-1" data-id="${item.id}">
+                    <button class="cart-remove-btn tap-press p-1" data-id="${item.id}">
                         <svg class="w-5 h-5 text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
                     </button>
                 </div>
